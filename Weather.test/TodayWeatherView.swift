0c1013339468d448f303400b4fd3424ef3856de5
Batutist <Batutist@gmail.com>
2017-11-08 00:52:52 +0300

    Set backgroundImage for TodayWeatherView. Add some layers and UIImages in main.Storyboard

---
+++ Weather.test/TodayWeatherView.swift
@@ -0,0 +1,22 @@
+//
+//  TodayWeatherView.swift
+//  Weather.test
+//
+//  Created by Ковалева on 07.11.17.
+//  Copyright © 2017 Ковалев. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TodayWeatherView: UIViewController {
+    @IBOutlet var myView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = Colors.skyBlue
+        
+    }
+    
+}
