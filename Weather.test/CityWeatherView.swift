2717282444ec16d8ed484df9e6c7e79c385d414c
Batutist <Batutist@gmail.com>
2017-11-08 11:03:47 +0300

    Creat file CityWeatherView and link it with CityWeatherView in main.Storyboard. change color to view

---
+++ Weather.test/CityWeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  CityWeatherView.swift
+//  Weather.test
+//
+//  Created by Ковалева on 08.11.17.
+//  Copyright © 2017 Ковалев. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CityWeatherView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = Colors.skyBlue
+    }
+    
+}
