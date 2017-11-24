02595c8e78710b712e5cbd2682246ec6b2c5c20a
Batutist <Batutist@gmail.com>
2017-11-25 00:15:14 +0300

    create animateBackgroudColor file

---
+++ Weather.test/AnimateBackgroudColor.swift
@@ -0,0 +1,18 @@
+//
+//  AnimateBackgroudGradient.swift
+//  Weather.test
+//
+//  Created by Ковалева on 25.11.17.
+//  Copyright © 2017 Ковалев. All rights reserved.
+//
+
+import Foundation
+
+class Animate
+{
+    func backgroundColor(of view: GradientView) {
+        
+    }
+    
+    
+}
