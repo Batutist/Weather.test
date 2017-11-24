8899f9c9b3997153d78b769972ef4000e719202b
Batutist <Batutist@gmail.com>
2017-11-25 00:25:38 +0300

    func animate backgroudColor

--- Weather.test/AnimateBackgroudColor.swift
+++ Weather.test/AnimateBackgroudColor.swift
@@ -7,11 +7,31 @@
 //
 
 import Foundation
+import UIKit
 
 class Animate
 {
     func backgroundColor(of view: GradientView) {
+        var colorsArray: [(color1: UIColor, color2: UIColor)] = []
+        var currentColorsArrayIndex = -1
         
+        colorsArray.append((color1: Color.orange, color2: Color.orangeLight))
+        colorsArray.append((color1: Color.orangeLight, color2: Color.mintGreen))
+        colorsArray.append((color1: Color.mintGreen, color2: Color.malachiteGreen))
+        colorsArray.append((color1: Color.malachiteGreen, color2: Color.pacificBlue))
+        colorsArray.append((color1: Color.pacificBlue, color2: Color.powderBlue))
+        colorsArray.append((color1: Color.powderBlue, color2: Color.violet))
+        colorsArray.append((color1: Color.violet, color2: Color.orchid))
+        colorsArray.append((color1: Color.orchid, color2: Color.orange))
+        
+        currentColorsArrayIndex = currentColorsArrayIndex == (colorsArray.count - 1) ? 0 : currentColorsArrayIndex + 1
+        
+        UIView.transition(with: view, duration: 2, options: [.transitionCrossDissolve], animations: {
+            view.firstColor = colorsArray[currentColorsArrayIndex].color1
+            view.secondColor = colorsArray[currentColorsArrayIndex].color2
+        }) { (success) in
+            self.backgroundColor(of: view)
+        }
     }
     
     
