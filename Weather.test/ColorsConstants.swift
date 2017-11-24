a243073b2721ed02292f59c1480029def1ee4077
Batutist <Batutist@gmail.com>
2017-11-24 22:43:44 +0300

    FIX colors values to right displaying in IU

--- Weather.test/ColorsConstants.swift
+++ Weather.test/ColorsConstants.swift
@@ -11,15 +11,15 @@ import UIKit
 
 struct Color {
     
-    static let orange = UIColor(red: 240, green: 127, blue: 41, alpha: 1)
-    static let orangeLight = UIColor(red: 246, green: 190, blue: 30, alpha: 1)
-    static let mintGreen = UIColor(red: 153, green: 255, blue: 179, alpha: 1)
-    static let malachiteGreen = UIColor(red: 0, green: 204, blue: 102, alpha: 1)
-    static let pacificBlue = UIColor(red: 0, green: 153, blue: 204, alpha: 1)
+    static let orange = UIColor(red: 240/255, green: 127/255, blue: 41/255, alpha: 1.0)
+    static let orangeLight = UIColor(red: 246/255, green: 190/255, blue: 30/255, alpha: 1.0)
+    static let mintGreen = UIColor(red: 153/255, green: 255/255, blue: 179/255, alpha: 1.0)
+    static let malachiteGreen = UIColor(red: 0/255, green: 204/255, blue: 102/255, alpha: 1.0)
+    static let pacificBlue = UIColor(red: 0/255, green: 153/255, blue: 204/255, alpha: 1.0)
     static let powderBlue = UIColor(red: 176/255, green: 224/255, blue: 230/255, alpha: 1.0)
     static let lightBlue = UIColor(red: 173/255, green: 216/255, blue: 230/255, alpha: 1.0)
     static let skyBlue = UIColor(red: 135/255, green: 206/255, blue: 235/255, alpha: 1.0)
-    static let violet = UIColor(red: 255, green: 153, blue: 255, alpha: 1)
-    static let orchid = UIColor(red: 214, green: 117, blue: 214, alpha: 1)
+    static let violet = UIColor(red: 255/255, green: 153/255, blue: 255/255, alpha: 1.0)
+    static let orchid = UIColor(red: 214/255, green: 117/255, blue: 214/255, alpha: 1.0)
     
 }
